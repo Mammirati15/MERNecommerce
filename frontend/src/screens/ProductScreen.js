a284911d8f9dfd8b07668c99e2609d62cde5f618
Matt Ammirati <matt.ammirati@gmail.com>
2021-03-24 11:57:59 -1000

    set up the backend with express

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -55,7 +55,7 @@ const ProductScreen = ({ match }) => {
                                 </Row>
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled={product.countInStock === 0} >
+                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
                                     Add to Cart
                                 </Button>
                             </ListGroup.Item>
