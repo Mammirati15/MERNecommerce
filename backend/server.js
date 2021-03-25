00a1e5b0d46bd892dec65418e6dd50f7e2d0a690
Matt Ammirati <matt.ammirati@gmail.com>
2021-03-25 12:18:51 -1000

    installed colors package manager to highlight messages in the console

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectdb from './config/db.js'
+import colors from 'colors'
 import products from './data/products.js'
 import connectDB from './config/db.js'
 
@@ -26,4 +26,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
